aad18a6084edff087e96559ce5e89f1a3eba6f86
Jephthah Dominic <stdominicjephthah63@gmail.com>
2024-09-09 15:30:03 +0100

    Built my portfolio, remainig the functionality for communicating with me

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,15 +7,15 @@ const Form = () => {
     <form action="" className='w-[70%] max-sm:w-full shadow-lg mt-5 p-10 border-2 border-theme-color border-opacity-60 rounded-md
     flex flex-col gap-3'>
         <div className='flex flex-col gap-1'>
-            <label htmlFor="name" className='text-white-500 text-xl font-openSans'>Name</label>
+            <label htmlFor="name" className='text-white-400 text-xl font-openSans'>Name</label>
             <input type="text" className='p-2 bg-chineseBlack rounded-md focus:border-none focus:bg-white outline-none'/>
         </div>
         <div className='flex flex-col gap-1'>
-            <label htmlFor="email" className='text-white-500 text-xl font-openSans'>Email</label>
+            <label htmlFor="email" className='text-white-400 text-xl font-openSans'>Email</label>
             <input type="text" className='p-2 bg-chineseBlack rounded-md focus:border-none focus:bg-white outline-none'/>
         </div>
         <div className='flex flex-col gap-1'>
-            <label htmlFor="message" className='text-white-500 text-xl font-openSans'>Message</label>
+            <label htmlFor="message" className='text-white-400 text-xl font-openSans'>Message</label>
             <textarea name="" id="" className='p-2 bg-chineseBlack h-[100px] rounded-md focus:border-none focus:bg-white outline-none resize-none'></textarea>
         </div>
         <div className='w-full flex justify-end mt-2'>
