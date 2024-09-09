aff35573f5bae5a4bd21b9a143e4aa8b0ec378d0
Jephthah Dominic <stdominicjephthah63@gmail.com>
2024-09-09 14:45:52 +0100

    Built my portfolio, remainig the functionalities and adding the urls for my socal media handle

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -10,8 +10,8 @@ const ProjectList = () => {
         <div className='w-full flex justify-center max-sm:flex-col gap-10 mt-10 max-xl:mt-0'>
           {projects.map((project)=>(
             <div className='relative cursor-pointer' key={project.url} onMouseOver={()=>setDisplay(project.url)} onMouseOut={()=>setDisplay("")}> 
-              <img src={project.img} alt="project snippet" className='xl:w-[700px] w-[300px] max-sm:w-full xl:h-[350px] h-[300px] brightness-60'/>
-              <div className={`absolute top-0 h-full w-full bg-black bg-opacity-50 flex flex-col justify-between ${display === project.url ? 'opacity-100 animate-fade-slow':'opacity-0'}`}>
+              <img src={project.img} alt="project snippet" className='xl:w-[700px] w-[300px] max-sm:w-full xl:h-[350px] h-[300px] brightness-60 object-cover'/>
+              <div className={`absolute top-0 h-full w-full bg-black bg-opacity-50 flex flex-col justify-between opacity-100`}>
                 <div className='w-full flex justify-end p-2'>
                   <IoLink className='text-white text-3xl'/>
                 </div>
