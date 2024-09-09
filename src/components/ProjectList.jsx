ba491612a951f1d2dc29b88e42735292c87bd9cf
Jephthah Dominic <stdominicjephthah63@gmail.com>
2024-09-09 14:50:17 +0100

    Built my portfolio, remainig the functionalities and adding the urls for my socal media handle

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -6,7 +6,7 @@ import { IoLink } from 'react-icons/io5';
 const ProjectList = () => {
     const [display, setDisplay] = useState(null);
     return (
-    <div className='w-full padding h-screen'>
+    <div className='w-full padding max-h-screen'>
         <div className='w-full flex justify-center max-sm:flex-col gap-10 mt-10 max-xl:mt-0'>
           {projects.map((project)=>(
             <div className='relative cursor-pointer' key={project.url} onMouseOver={()=>setDisplay(project.url)} onMouseOut={()=>setDisplay("")}> 
