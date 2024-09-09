b60a3ee9a7a51808dcf99e94f3ae2f5f4c92d26e
Jephthah Dominic <stdominicjephthah63@gmail.com>
2024-09-09 14:47:01 +0100

    Built my portfolio, remainig the functionalities and adding the urls for my socal media handle

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
   
   return (
     <section className='w-full bg-eerie-black py-28 xl:padding' id='portfolio'>
-      <h1 className='font-openSans text-3xl text-white-500 text-center'>LATEST <span className='border-b-2 border-b-blue-300'>PROJECTS</span></h1>
+      <h1 className='font-openSans text-3xl text-white-400 text-center'>LATEST <span className='border-b-2 border-b-blue-300'>PROJECTS</span></h1>
       <ProjectList />
     </section>
   )
